9b1e75883c71de846a62648886287f7718582699
jamesanthonyferguson <jamesanthonyferguson>
2014-09-12 16:01:20 -0700

    Wrote the bloom filter and made sure it produced false positives within 10% of the theoretical rate.

--- sprint-two/spec/bloomFilterSpec.js
+++ sprint-two/spec/bloomFilterSpec.js
@@ -6,8 +6,47 @@ describe('bloomFilter', function() {
     bloomFilter = new BloomFilter(18);
   });
 
-  it('be able to hash stuff', function() {
-    bloomFilter.hash("something");
+  it('should have the theoretical false positive rate', function() {
+    var createRandomWord = function(length) {
+      var word = '';
+      for (var x = 0; x < 9; x++) {
+        word += String.fromCharCode(Math.floor(Math.random()*26) + 97);
+      }
+      return word;
+    }
+    var n = 6;
+    var wordLength = 8;
+    var insertedWords = [];
+    var total = 100000;
+    var falsePositives = 0;
+    //insert words
+    for (var i = 0; i < n; i++) {
+      var word = createRandomWord(wordLength);
+      insertedWords.push(word);
+      bloomFilter.insert(word);
+    }
+    //check random words
+    var truePositives = 0;
+    for (var i = 0; i < total; i++) {
+      var word = createRandomWord(wordLength);
+      if (bloomFilter.contains(word)) {
+        if (_.contains(insertedWords, word) === false) {
+          falsePositives++;
+        } else {
+          truePositives++;
+        }
+      }
+    }
+    total -= truePositives;
+
+    var falsePositiveRate = falsePositives / total;
+    var theoreticalRate = Math.pow( (1 - Math.pow((1 - 1/bloomFilter.m), (bloomFilter.k * n))), bloomFilter.k);
+    console.log("Welcome to the bloom Filter!")
+    console.log("Total false positives: " + falsePositives);
+    console.log("False positive Rate: " + falsePositiveRate);
+    console.log("Theoretical false positive rate: " +  theoreticalRate);
+
+    expect((theoreticalRate > falsePositiveRate*0.9)&&(theoreticalRate < falsePositiveRate*1.1)).to.equal(true);
   });
 
 });
--- sprint-two/src/bloomFilter.js
+++ sprint-two/src/bloomFilter.js
@@ -1,11 +1,36 @@
 var BloomFilter = function(limit) {
-  //this._storage = BloomFilter.makeLimitedArray(limit);
+  this.k = 4;
+  this.m = 100;
+  this.storage = [];
+  for (var i = 0; i < this.m; i++) {
+    this.storage[i] = 0;
+  }
 }
 
 BloomFilter.prototype.hash = function(message) {
-  console.log(Sha1.hash(message).length);
+  var sha1hash = Sha1.hash(message);
+  var hashes = [];
+  var hashLength = Math.floor(sha1hash.length / this.k);
+  for (var i = 0; i < this.k; i++) {
+    hashes[i] = sha1hash.substring(i*hashLength,(i+1)*hashLength);
+    hashes[i] = parseInt(hashes[i],16) % this.m;
+  }
+  return hashes;
 }
 
+BloomFilter.prototype.insert = function(elmt) {
+  var hashes = this.hash(elmt.toString());
+  for (var i = 0; i < hashes.length; i++) {
+    this.storage[hashes[i]] = 1;
+  }
+};
 
-//HelperFunctions
-BloomFilter.prototype.makeLimitedArray = function(limit) {};
+BloomFilter.prototype.contains = function(elmt) {
+  var hashes = this.hash(elmt.toString());
+  for (var i = 0; i < hashes.length; i++) {
+    if(this.storage[hashes[i]] === 0) {
+      return false;
+    }
+  }
+  return true;
+};
