acbeb4fada27c567570410b525e74c7248e91989
jamesanthonyferguson <jamesanthonyferguson>
2014-09-13 12:45:41 -0700

    working

--- sprint-two/src/bTree.js
+++ sprint-two/src/bTree.js
@@ -1,29 +1,11 @@
 var BTree = function (val, parent) {
-  this.val1 = val;
-  this.val2 = undefined;
-  tree.leftLink = undefined;
-  tree.midLink = undefined;
-  tree.rightLink = undefined;
+  this.val = [];
+  this.children = [null,null,null,null,null]
   this.parent = parent;
 }
 
-
 BTree.prototype.insert = function (val) {
-  //Case 1
-  if (val === this.val1 || val === this.val2) {
-    break;
-  } else if (this.val2 === undefined){
-    if (val < this.val1) {
-      this.val2 = this.val1
-      this.val1 = val
-    } else if (val > this.val1) {
-      this.val2 = val;
-    }
-  } else if (val < this.val) {
 
-  }
-  //Case 2
-  //Case 3 - left, mid, right
 };
 
 BTree.prototype.contains = function(val) {};
@@ -32,23 +14,38 @@ BTree.prototype.depthFirstLog = function(callback) {};
 
 BTree.prototype.breadthFirstLog = function(callback) {};
 
-//Insert pseudo:
-// if the value is found as either of the two values in a node, break
-// otherwise, if the current leaf node is not full
-  // check which value position it should be in
-  // and insert it
+
+//this.children explanation: [illegitimate,legit,legit,legit,illegitimate]
+
+//addTree pseudo:
+//traverse down tree until links are undefined, - if val === element of this.val, should not flow;
+// if the current leaf node is not full (this.val.length <2)
+  // if smaller than first element
+    // shift it
+  // if greater
+    //push it
 // if current leaf node is full
-  // find low, middle and high values
-  //if parent node exists with one value
-    //call insert on parent node passing in middle value
+  // find position in val.array for val to be inserted
+  // insert it
+  //if parent node exists with one value and therefore has .children type [null,x,y,null,null]
+
+    //!!!!split the child node!!!!:
     //if child is LHS of parent
-      //remove the high value from the original leaf node
-      //make the midlink of parent a tree with high as the passed in val
+      //move x and y in the parent.children to the right so now [null,null,x,y,null]
+      //remove the val[2] (pop) from the original leaf node
+      //insert the popped val[2] as tree at parent.children[1]
     //if child is RHS of parent
-      //remove the low value from the original leaf node
-      //make the midlink of parent a tree with high as the passed in val
+      //remove the val[2] (pop) from the original leaf node
+      //insert the popped val[2] as tree at parent.children[3]
+      ///////////////////////////////////////YOU ARE UP TO HERE
+    //call addTree on parent node passing in middle value
   //if parent node exists and is full
+    //split, then push
     //?? call insert on it, passing in middle??
-  // if parent node does not exist:
     //
+  // if parent node does not exist:
+    //make a new tree using the middle value
+    //make a new tree with the val2 of the childnode as its argument
+    //make a new
+    //set childnode to be the left link
   // chan
